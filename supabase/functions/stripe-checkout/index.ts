b2efa5d4eb1554682531a0f926229476248cce32
Samuel Warrie <104434957+samuel-warrie@users.noreply.github.com>
2026-03-17 13:10:05 +0200

    Fix Stripe checkout initialization

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -5,6 +5,7 @@ import { createClient } from 'npm:@supabase/supabase-js@2.49.1';
 const supabase = createClient(Deno.env.get('SUPABASE_URL') ?? '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '');
 const stripeSecret = Deno.env.get('STRIPE_SECRET_KEY')!;
 const stripe = new Stripe(stripeSecret, {
+  apiVersion: '2024-12-18.acacia',
   appInfo: {
     name: 'Bolt Integration',
     version: '1.0.0',
